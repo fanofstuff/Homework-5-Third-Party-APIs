7ccf638b78310ef05ea57a9273792cd4835d91c6
Tucker Hembree <bladeofxp@gmail.com>
2020-03-13 13:45:18 -0400

    created basic HTML structure using Bootstrap container and grid; added pre-existing classes and bootstrap classes where necessary; updated style and css links to account for changed file structure; currently experimenting with getting javascript .getHours() function to work with an Interval to auto-update; will reconfirm path after asking teacher for minor guidance - is Interval really a good idea, here?

---
+++ Assets/Scripts/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+var dt = new Date(); 
+var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds(); 
+console.log (time); 
+
+
+// var updateTime = function ()
+
+});  
+
+
+
+
+
+
+
+
+
+
+
